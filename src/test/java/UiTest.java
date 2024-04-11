f6e5027c7ef1a599a75250bdd9ced5b1b4c1ebeb
idea <idea@mail.ru>
2024-04-11 15:09:28 +0300

    Was added parametrized test

--- src/test/java/UiTest.java
+++ src/test/java/UiTest.java
@@ -1,5 +1,7 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,9 +18,14 @@ import static org.openqa.selenium.By.xpath;
 
 public class UiTest {
 
-    @Test
-    public void test() {
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver = new ChromeDriver();
+    private static final String URL = "https://www.youtube.com/results?search_query=";
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"Mr.Beast",
+            "Nicki Minaj"})
+    public void test(String name) {
         driver.get("https://www.google.ru");
         WebElement finder = driver.findElement(By.xpath("//textarea"));
         finder.sendKeys("YouTube");
@@ -52,7 +59,7 @@ public class UiTest {
 
         Wait<WebDriver> wait2 = new WebDriverWait(driver, Duration.ofSeconds(2));
         wait2.until(d -> youTubeSearch.isDisplayed());
-        youTubeSearch.sendKeys("Nicki Minaj");
+        youTubeSearch.sendKeys(name);
 
         WebElement clickNicki = driver.findElement(xpath("//button[@id = 'search-icon-legacy']"));
 
@@ -60,16 +67,27 @@ public class UiTest {
         wait3.until(d -> clickNicki.isDisplayed());
         clickNicki.click();
 
-        Wait<WebDriver> wait4 = new WebDriverWait(driver, Duration.ofSeconds(5));
+        Wait<WebDriver> wait4 = new WebDriverWait(driver, Duration.ofSeconds(2));
         wait4.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@aria-label='Фильтры']")));
 
-
-        Assertions.assertEquals("https://www.youtube.com/results?search_query=Nicki+Minaj", driver.getCurrentUrl(),
-                "Expected:" + "https://www.youtube.com/results?search_query=Nicki+Minaj" + " actual: " +
+        String newName;
+        if (name.contains(" ")) {
+            newName = name.replace(" ", "+");
+        } else {
+            return;
+        }
+        Assertions.assertEquals(URL + newName, driver.getCurrentUrl(),
+                "Expected:" + URL + name + " actual: " +
                         driver.getCurrentUrl());
 
         WebElement submit = driver.findElement(xpath("//div[@id='subscribe-button'][contains(@class,'ytd-watch-metadata')]"));
-        Assertions.assertTrue(driver.getCurrentUrl().contains(submit.getText()));
+        Assertions.assertTrue(driver.getCurrentUrl().contains(submit.getText()), "Contains button submit");
+    }
+
+    @AfterEach
+    public void finish() {
+        driver.quit();
+
     }
 }
 
